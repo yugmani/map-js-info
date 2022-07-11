098bf57cabec03512a7a1b23576de1c1869a0e94
yugmani <yugmani@yahoo.com>
2022-07-10 22:20:13 -0700

    create map, set keys and values and get the values by keys

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 // Import stylesheets
 import './style.css';
 
+// Creating a new map
+let myMap = new Map();
+
+// Storing a value by the key;
+myMap.set('1', 'my string 1'); // a string key
+myMap.set(2, 'my number 2'); // a numeric key
+myMap.set(true, 'isCompleted'); // a boolean key
+
+//Getting the value by the key,
+const key1 = myMap.get('1');
+console.log(key1); //my string 1
+const key2 = myMap.get(2);
+console.log(key2); //my number 2
+
+//checking if a key exists in the given map.
+let check = myMap.has('1');
+console.log(check); //true;
